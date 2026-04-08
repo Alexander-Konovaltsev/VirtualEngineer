2069aa2004a78298de7687156c1d1490cddb029c
Alexander-Konovaltsev <aleksandar.konovaltseff@yandex.ru>
2026-04-08 22:35:52 +0300

    Добавляет модель сцены

---
+++ Assets/Scripts/Models/Scene.cs
@@ -0,0 +1,10 @@
+namespace VirtualEngineer.Models
+{
+    public class Scene
+    {
+        public int id;
+        public string name;
+        public string title;
+        public string description;
+    }
+}
