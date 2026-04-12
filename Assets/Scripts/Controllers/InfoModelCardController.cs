9079e0cfd807f6cbb8ed4d9fb7d34464aa4cc0ff
Alexander-Konovaltsev <aleksandar.konovaltseff@yandex.ru>
2026-04-12 22:06:32 +0300

    добавляет следование информационного окна за пользователем

--- Assets/Scripts/Controllers/InfoModelCardController.cs
+++ Assets/Scripts/Controllers/InfoModelCardController.cs
@@ -10,6 +10,36 @@ namespace VirtualEngineer.Controllers
         [SerializeField]
         private TMP_Text descripiton;
 
+        private Transform cameraTransform;
+        private Vector3 basePosition;
+
+        private void Start()
+        {
+            cameraTransform = Camera.main.transform;
+            basePosition = transform.position;
+        }
+
+        private void LateUpdate()
+        {
+            if (cameraTransform == null) return;
+
+            Vector3 dirToCamera = (cameraTransform.position - basePosition).normalized;
+
+            float offset = 0.5f;
+            float heightOffset = 0.15f;
+
+            Vector3 targetPos = basePosition + dirToCamera * offset;
+            targetPos.y += heightOffset;
+
+            Vector3 lookPos = cameraTransform.position;
+            lookPos.y = targetPos.y;
+
+            transform.position = targetPos;
+
+            transform.LookAt(lookPos);
+            transform.Rotate(0, 180f, 0);
+        }
+  
         public void Init(string title, string descripiton)
         {
             this.title.text = title;
